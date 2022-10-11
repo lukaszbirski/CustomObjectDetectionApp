e8619d59af0d48d277f7cf2598a3abd0e9790556
Lukasz <lukasz.birski@gmail.com>
2022-10-11 21:31:41 +0200

    Added image picker

--- app/src/main/java/pl/birski/customobjectdetectionapp/MainActivity.kt
+++ app/src/main/java/pl/birski/customobjectdetectionapp/MainActivity.kt
@@ -1,11 +1,33 @@
 package pl.birski.customobjectdetectionapp
 
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts.PickVisualMedia
 import androidx.appcompat.app.AppCompatActivity
+import pl.birski.customobjectdetectionapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.selectImageBtn.setOnClickListener {
+            pickMedia.launch(PickVisualMediaRequest(PickVisualMedia.ImageOnly))
+        }
+
+        setContentView(binding.root)
     }
+
+    private val pickMedia = registerForActivityResult(PickVisualMedia()) { uri ->
+        uri?.let {
+            binding.imageView.setImageBitmap(getBitmap(it))
+        }
+    }
+
+    private fun getBitmap(uri: Uri) = MediaStore.Images.Media.getBitmap(this.contentResolver, uri)
 }
